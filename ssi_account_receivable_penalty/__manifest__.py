252697b9351adbe9b2ee7bf5077ff0c7adc3ac81
Andhitia Rama <andhitia.r@gmail.com>
2025-04-18 09:03:51 +0700

    [UPD] ssi_account_receivable_penalty
    * Merubah lisensi dari LGPL menjadi AGPL pd semua file

--- ssi_account_receivable_penalty/__manifest__.py
+++ ssi_account_receivable_penalty/__manifest__.py
@@ -1,6 +1,6 @@
 # Copyright 2022 OpenSynergy Indonesia
 # Copyright 2022 PT. Simetri Sinergi Indonesia
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Receivable Penalty",
     "version": "14.0.1.0.1",

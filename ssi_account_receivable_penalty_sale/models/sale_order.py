36dd733b359f8454c4c67844ce54036a2db9f4bc
Andhitia Rama <andhitia.r@gmail.com>
2025-05-07 08:52:16 +0700

    [UPD] ssi_account_receivable_penalty_sale
    * Bug fix

--- ssi_account_receivable_penalty_sale/models/sale_order.py
+++ ssi_account_receivable_penalty_sale/models/sale_order.py
@@ -56,7 +56,12 @@ class SaleOrder(models.Model):
 
     def _compute_receivable_penalty_ids(self):
         for record in self:
-            result = record.mapped("invoice_ids.receivable_penalty_ids.id")
+            result = []
+            if record.invoice_ids:
+                for invoice in record.invoice_ids:
+                    if invoice.receivable_penalty_ids:
+                        for penalty in invoice.receivable_penalty_ids:
+                            result.append(penalty.id)
             record.receivable_penalty_ids = result
 
     def action_view_penalty(self):
